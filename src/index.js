e575a83e89f775c8b17f93b49d890e359dd7ba7a
danielbeyene <daniel.beyene@student.moringashcool.com>
2024-04-20 14:43:19 +0300

    Challenge: TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const createTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const tasksList = document.getElementById("tasks");
+
+  createTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (newTaskDescription.value.trim()) {
+      const newTask = document.createElement("li");
+      newTask.textContent = newTaskDescription.value.trim();
+      tasksList.appendChild(newTask);
+      newTaskDescription.value = "";
+    }
+  });
 });
